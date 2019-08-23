176ca3248a894311dbf196a19179b410705f22ae
Tanya Bihag <tbihag@gmail.com>
2019-08-23 02:34:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,20 @@ languages = ['ruby', 'javascript', 'python', 'objective-c']
 students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
 
 def my_collect(collection)
-  empty_array = []
+  empty_array = [ ]
   i = 0 
   my_collect(empty_array) { |i| i += 1 }
   empty_array
+end
+
+
+
+def hello(array)
+  i = 0
+  collection = [ ]
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
