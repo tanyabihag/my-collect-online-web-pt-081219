3ab027f022704a15409819f18c58cc577dfb449a
Tanya Bihag <tbihag@gmail.com>
2019-08-23 02:39:27 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
+
+def my_collect(array)
+  i = 0
+  collection = [ ]
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
+
+
 
 
