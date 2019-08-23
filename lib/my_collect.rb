1d0e05912d18f4f8a64b65be848ea821918c2018
Tanya Bihag <tbihag@gmail.com>
2019-08-23 02:33:50 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller'] 
 
-
+def my_collect(collection)
+  empty_array = []
+  i = 0 
+  my_collect(empty_array) { |i| i += 1 }
+  empty_array
+end
